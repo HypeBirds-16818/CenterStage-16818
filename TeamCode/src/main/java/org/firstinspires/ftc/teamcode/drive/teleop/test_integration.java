4e58b9640c6e40377122c4c0255c4ab8e52c1f5d
rangel-cgh <coshe1111@gmail.com>
2023-12-04 17:50:24 -0300

    Update test_integration.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/test_integration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/test_integration.java
@@ -3,22 +3,37 @@ package org.firstinspires.ftc.teamcode.drive.teleop;
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @TeleOp(name = "Integrated Test")
 public class test_integration extends LinearOpMode {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+    CRServo planeServo;
+
 
     @Override
     public void runOpMode() throws InterruptedException {
         drive.setModeIntake(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        planeServo = hardwareMap.get(CRServo.class, "planeServo");
         waitForStart();
 
         while (opModeIsActive() && !isStopRequested()) {
             setPid(drive, "Top", 100);
+
+            if(gamepad1.a){
+                planeServo.setPower(1);
+            }
+            if(gamepad1.b){
+                planeServo.setPower(-1);
+            }
+            if(gamepad1.x){
+                planeServo.setPower(0);
+            }
         }
     }
 
