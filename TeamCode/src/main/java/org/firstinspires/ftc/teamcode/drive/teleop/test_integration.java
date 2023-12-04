11a00c41e2d88a5f86e196848384dd6278bb3ea6
rangel-cgh <coshe1111@gmail.com>
2023-12-04 17:34:35 -0300

    Update test_integration.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/test_integration.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/teleop/test_integration.java
@@ -2,25 +2,31 @@ package org.firstinspires.ftc.teamcode.drive.teleop;
 
 import com.arcrobotics.ftclib.controller.PIDController;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
+@TeleOp(name = "Integrated Test")
 public class test_integration extends LinearOpMode {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
-
-
     @Override
     public void runOpMode() throws InterruptedException {
-        setPid(drive, "Top", 100);
+        drive.setModeIntake(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        waitForStart();
+
+        while (opModeIsActive() && !isStopRequested()) {
+            setPid(drive, "Top", 100);
+        }
     }
 
     public void setPid(SampleMecanumDrive drive, String PIDcase, int target){
-        if (PIDcase == "Top"){
+        if (PIDcase.equals("Top")){
             drive.getTopPID(target);
         }
-        else if (PIDcase == "Bottom"){
+        else if (PIDcase.equals("Bottom")){
             drive.getBottomPID(target);
         }
 
